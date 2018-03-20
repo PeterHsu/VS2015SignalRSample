9f0ed922c2796d0855a03c3280bb319bd644bbd7
Peter Hsu <pthsu@outlook.com>
2018-03-20 12:10:02 +0800

    ,

---
+++ AngularClient/src/app/app.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+declare var $: any;
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit {
+  title = 'app';
+  ngOnInit() {
+    $.connection.hub.url = 'http://localhost:57319/signalr';
+    // Declare a proxy to reference the hub.
+    const chat = $.connection.chatHub;
+    // Create a function that the hub can call to broadcast messages.
+    chat.client.broadcastMessage = function (name, message) {
+        // Html encode display name and message.
+        const encodedName = $('<div />').text(name).html();
+        const encodedMsg = $('<div />').text(message).html();
+        // Add the message to the page.
+        $('#discussion').append('<li><strong>' + encodedName
+            + '</strong>:&nbsp;&nbsp;' + encodedMsg + '</li>');
+    };
+    // Get the user name and store it to prepend to messages.
+    $('#displayname').val(prompt('Enter your name:', ''));
+    // Set initial focus to message input box.
+    $('#message').focus();
+    // Start the connection.
+    $.connection.hub.start({ jsonp: true }).done(function () {
+        $('#sendmessage').click(function () {
+            // Call the Send method on the hub.
+            chat.server.send($('#displayname').val(), $('#message').val());
+            // Clear text box and reset focus for next comment.
+            $('#message').val('').focus();
+        });
+    });
+  }
+}
